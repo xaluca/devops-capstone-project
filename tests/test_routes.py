ab9656eacb786dbeb69252a8128547c0912dfec9
xaluca <jaufre.charuel@gmail.com>
2023-04-17 06:43:27 -0400

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,7 +138,7 @@ class TestAccountService(TestCase):
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-        def test_get_account_list(self):
+    def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
         resp = self.client.get(BASE_URL)
